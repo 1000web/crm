5528a13a74ed54f228fd9cde814156335bb33011
Evgeniy Dmitrichenko <admin@1000web.ru>
2013-07-30 15:57:48 +0400

    Добавлен OrganizationGroup logging.

--- protected/controllers/OrganizationGroupController.php
+++ protected/controllers/OrganizationGroupController.php
@@ -20,6 +20,7 @@ class OrganizationGroupController extends Controller
 	public function actionCreate()
 	{
 		$model=new OrganizationGroup;
+		$model_log=new OrganizationGroupLog;
 
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
@@ -27,8 +28,11 @@ class OrganizationGroupController extends Controller
 		if(isset($_POST['OrganizationGroup']))
 		{
 			$model->attributes=$_POST['OrganizationGroup'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
+			if($model->save()){
+                $model_log->attributes = $model->attributes;
+                $model_log->save();
+                $this->redirect(array('view','id'=>$model->id));
+            }
 		}
 
 		$this->render('create',array(
@@ -44,6 +48,7 @@ class OrganizationGroupController extends Controller
 	public function actionUpdate($id)
 	{
 		$model=$this->loadModel($id);
+        $model_log=new OrganizationGroupLog;
 
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
@@ -51,8 +56,11 @@ class OrganizationGroupController extends Controller
 		if(isset($_POST['OrganizationGroup']))
 		{
 			$model->attributes=$_POST['OrganizationGroup'];
-			if($model->save())
-				$this->redirect(array('view','id'=>$model->id));
+			if($model->save()){
+                $model_log->attributes = $model->attributes;
+                $model_log->save();
+                $this->redirect(array('view','id'=>$model->id));
+            }
 		}
 
 		$this->render('update',array(
@@ -67,11 +75,17 @@ class OrganizationGroupController extends Controller
 	 */
 	public function actionDelete($id)
 	{
-		$this->loadModel($id)->delete();
+        //$this->loadModel($id)->delete();
+        $model = $this->loadModel($id);
+        $model_log = new OrganizationGroupLog;
+        $model_log->attributes = $model->attributes;
+        $model_log->setAttribute('deleted', 1);
+        $model_log->save();
+        $model->delete();
 
 		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
 		if(!isset($_GET['ajax']))
-			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
+			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('index'));
 	}
 
 	/**
