b94333bcc736dcf5f427bce22c3faf984afdaaae
Evgeniy Dmitrichenko <admin@1000web.ru>
2013-08-07 05:04:53 +0400

    Check access for each item in Operatios Menu.

--- protected/views/site/index.php
+++ protected/views/site/index.php
@@ -13,9 +13,11 @@ else $this->buildPageOptions($model);
         <h3>Клиенты</h3>
         <p>Клиенты - это люди в компании, с которыми вы общаетесь и взаимодействуете в рамках достижения бизнес-задач.</p>
         <p>
-            <a href="/customer/create">Создать</a>
-            или
-            <a href="/customer">Список</a>
+            <?php
+            if(Yii::app()->user->checkAccess('customer.create')) echo '<a href="/customer/create">Создать</a>&nbsp;&nbsp;&nbsp;';
+            if(Yii::app()->user->checkAccess('customer.index')) echo '<a href="/customer/index">Список</a>&nbsp;&nbsp;&nbsp;';
+            if(Yii::app()->user->checkAccess('customer.admin')) echo '<a href="/customer/admin">Управление</a>&nbsp;&nbsp;&nbsp;';
+            ?>
         </p>
     </td>
 </tr>
@@ -25,9 +27,11 @@ else $this->buildPageOptions($model);
         <h3>Организации</h3>
         <p>Организации - это компании или корпоративные отделы, с которыми вы имеете деловые отношения.</p>
         <p>
-            <a href="/organization/create">Создать</a>
-            или
-            <a href="/organization">Список</a>
+            <?php
+            if(Yii::app()->user->checkAccess('organization.create')) echo '<a href="/organization/create">Создать</a>&nbsp;&nbsp;&nbsp;';
+            if(Yii::app()->user->checkAccess('organization.index')) echo '<a href="/organization/index">Список</a>&nbsp;&nbsp;&nbsp;';
+            if(Yii::app()->user->checkAccess('organization.admin')) echo '<a href="/organization/admin">Управление</a>&nbsp;&nbsp;&nbsp;';
+            ?>
         </p>
     </td>
 </tr>
@@ -37,9 +41,11 @@ else $this->buildPageOptions($model);
         <h3>Задачи</h3>
         <p>Задачи - перечень или реестр задач, событий и звонков, связанных с записями CRM, относящимися к различным модулям.</p>
         <p>
-            <a href="/task/create">Создать</a>
-            или
-            <a href="/task">Список</a>
+            <?php
+            if(Yii::app()->user->checkAccess('task.create')) echo '<a href="/task/create">Создать</a>&nbsp;&nbsp;&nbsp;';
+            if(Yii::app()->user->checkAccess('task.index')) echo '<a href="/task/index">Список</a>&nbsp;&nbsp;&nbsp;';
+            if(Yii::app()->user->checkAccess('task.admin')) echo '<a href="/task/admin">Управление</a>&nbsp;&nbsp;&nbsp;';
+            ?>
         </p>
     </td>
 </tr>
@@ -49,9 +55,11 @@ else $this->buildPageOptions($model);
         <h3>Потенциальные сделки</h3>
         <p>Потенциальные сделки - это сделки с организациями или людьми, приносящие доход вашей организации.</p>
         <p>
-            <a href="/deal/create">Создать</a>
-            или
-            <a href="/deal">Список</a>
+            <?php
+            if(Yii::app()->user->checkAccess('deal.create')) echo '<a href="/deal/create">Создать</a>&nbsp;&nbsp;&nbsp;';
+            if(Yii::app()->user->checkAccess('deal.index')) echo '<a href="/deal/index">Список</a>&nbsp;&nbsp;&nbsp;';
+            if(Yii::app()->user->checkAccess('deal.admin')) echo '<a href="/deal/admin">Управление</a>&nbsp;&nbsp;&nbsp;';
+            ?>
         </p>
     </td>
 </tr>
