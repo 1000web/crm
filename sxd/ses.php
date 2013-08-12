f47928b5708a3233cc46445d0021e0b8002bc06f
Evgeniy Dmitrichenko <admin@1000web.ru>
2013-08-12 16:41:56 +0400

    DB dump 2013-08-12 16:40

--- sxd/ses.php
+++ sxd/ses.php
@@ -1,6 +1,6 @@
 <?php
 $SES = array (
-  'a679409b55991acf4c51dd29f82bc077' => 
+  '910ae0e94b86add93c446842a7550d48' => 
   array (
     'cfg' => 
     array (
@@ -13,7 +13,7 @@ $SES = array (
       'only_create' => 'MRG_MyISAM MERGE HEAP MEMORY',
       'globstat' => 0,
       'my_host' => 'localhost',
-      'my_port' => 3306,
+      'my_port' => '8306',
       'my_user' => 'root',
       'my_pass' => 'root',
       'my_comp' => 0,
@@ -24,7 +24,7 @@ $SES = array (
       'confirm' => '6',
       'exitURL' => './',
     ),
-    'time' => 1376059640,
+    'time' => 1376298987,
     'lng' => 'ru',
   ),
 );
