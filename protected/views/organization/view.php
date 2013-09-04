3e42a8e3af026d24ddfa48f71fcae99ff15bbac7
Evgeniy Dmitrichenko <admin@1000web.ru>
2013-09-04 13:11:32 +0400

    Organization View detailed list fixed (Type, Group, Region).

--- protected/views/organization/view.php
+++ protected/views/organization/view.php
@@ -5,28 +5,15 @@
 /* @var $customer Customer */
 /* @var $deal Deal */
 
-/*
-$attr = array(
-//        array('name' => 'id', 'label' => '#'),
-        //array('name' => 'organization_type_id', 'label' => 'Тип'),
-        //array('name' => 'organizationType->value', 'label' => 'Тип'),
-        array('name' => 'organization_type_id', 'label' => 'Тип', 'value' => $model->organizationType->value),
-        array('name' => 'organization_group_id', 'label' => 'Группа', 'value' => $model->organizationGroup->value),
-        array('name' => 'organization_region_id', 'label' => 'Регион', 'value' => $model->organizationRegion->value),
-        //array('name' => 'value', 'label' => 'Название'),
-        array('name' => 'description', 'label' => 'Описание'),
-);/**/
-$this->addAttributes(array(
-    'organization_type_id',
-    'organization_group_id',
-    'organization_region_id',
-    'description',
-));
+$this->addAttribute('organization_type_id',$model->organizationType->value);
+$this->addAttribute('organization_group_id',$model->organizationGroup->value);
+$this->addAttribute('organization_region_id',$model->organizationRegion->value);
+$this->addAttribute('description');
+
 
 echo $this->renderPartial('../detail_view', array(
     'dataProvider' => $model,
 ));
-
 
 if ($this->checkAccess('organizationcontact', 'view')) {
     echo '<h2>Контакты Организации</h2>';
@@ -34,14 +21,12 @@ if ($this->checkAccess('organizationcontact', 'view')) {
         'dataProvider' => $contact,
     ));
 }
-
 if ($this->checkAccess('customer', 'view')) {
     echo '<h2>Клиенты Организации</h2>';
     echo $this->renderPartial('../customer/index', array(
         'dataProvider' => $customer,
     ));
 }
-
 if ($this->checkAccess('deal', 'view')) {
     echo '<h2>Сделки Организации</h2>';
     echo $this->renderPartial('../deal/index', array(
