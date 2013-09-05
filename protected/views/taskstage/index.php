7d10e474549c80e4c94c43814a3291e28f7dabc1
Evgeniy Dmitrichenko <admin@1000web.ru>
2013-09-05 12:42:21 +0400

    Task buttons in type, prior, stage are fixed.

--- protected/views/taskstage/index.php
+++ protected/views/taskstage/index.php
@@ -3,7 +3,7 @@
 /* @var $dataProvider CActiveDataProvider */
 
 $this->buttons = $this->columns = array();
-$this->addButtons('product', array('view', 'update', 'delete', 'log'));
+$this->addButtons('taskstage', array('view', 'update', 'delete', 'log'));
 $this->addColumns(array('prior', 'value', 'description'));
 
 echo $this->renderPartial('../grid_view', array(
